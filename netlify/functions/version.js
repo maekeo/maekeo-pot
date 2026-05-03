0a7899eae9df3821af2ff12a952f57ce56a1227a
maekeo <maeilcoffees@gmail.com>
2026-05-03 23:13:44 +0900

    Add files via upload

--- netlify/functions/version.js
+++ netlify/functions/version.js
@@ -1,11 +1,9 @@
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-      'Cache-Control': 'no-cache, no-store, must-revalidate',
-      'Access-Control-Allow-Origin': '*',
-    },
-    body: JSON.stringify({ version: 'v1.1.2' }),
-  };
-};
+exports.handler = async () => ({
+  statusCode: 200,
+  headers: {
+    'Content-Type': 'application/json',
+    'Cache-Control': 'no-cache, no-store, must-revalidate',
+    'Access-Control-Allow-Origin': '*',
+  },
+  body: JSON.stringify({ version: 'v1.1.2' }),
+});
