e79051c4c2326e9e3d97bc6276a9c429f772a0cc
maekeo <maeilcoffees@gmail.com>
2026-07-22 22:40:03 +0900

    Add files via upload

--- netlify/functions/version.js
+++ netlify/functions/version.js
@@ -5,5 +5,5 @@ exports.handler = async () => ({
     'Cache-Control': 'no-cache, no-store, must-revalidate',
     'Access-Control-Allow-Origin': '*',
   },
-  body: JSON.stringify({ version: 'v1.8.2' }),
+  body: JSON.stringify({ version: 'v1.8.3' }),
 });
